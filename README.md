b0ef3f63362728975f8d2ab9c80c4983c0c382a1
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 13:28:28 -0700

    initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# More Practice Read Me
---
+++ practice.sql
@@ -0,0 +1,15 @@
+-- //////////////////////////////////////////////////////
+-- Coding Dojo > Python Stack > mySQL > Queries: More Practice
+-- By: Virgilio D. Cabading Jr.     Created: November 2, 2021
+-- //////////////////////////////////////////////////////
+
+-- CREATE
+
+
+-- RETRIEVE
+
+
+-- UPDATE
+
+
+-- DELETE
